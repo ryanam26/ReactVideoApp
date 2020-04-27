edeacdf727633410b9d1ff88f31cda31f5203d8e
Ryan Morrison <ryanam26@yahoo.com>
2020-04-27 14:14:06 -0400

    added video

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from 'react-dom';
+import { Video } from './Video';
+import { Menu } from './Menu';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          My Awesome App.
-        </p>
-   
-      </header>
-    </div>
-  );
+const VIDEOS = {
+  fast: 'https://s3.amazonaws.com/codecademy-content/courses/React/react_video-fast.mp4',
+  slow: 'https://s3.amazonaws.com/codecademy-content/courses/React/react_video-slow.mp4',
+  cute: 'https://s3.amazonaws.com/codecademy-content/courses/React/react_video-cute.mp4',
+  eek: 'https://s3.amazonaws.com/codecademy-content/courses/React/react_video-eek.mp4'
+};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { src: VIDEOS.fast };
+    this.chooseVideo = this.chooseVideo.bind(this)
+  }
+  
+  chooseVideo(newVideo) {
+    this.setState({
+      src: VIDEOS[newVideo]
+    })
+  }
+  
+  render() {
+    return (
+      <div>
+        <h1>Video Player</h1>
+        <Menu chooseVideo={this.chooseVideo}/>
+        <Video src={this.state.src}/>
+      </div>
+    );
+  }
 }
 
 export default App;
